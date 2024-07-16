1610ba90e8079d5d58fa82aa46696ea3b43b690a
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2024-07-15 22:41:19 -0400

    use `NBITS=16` when saving LOS files (#51)

--- src/opera_utils/constants.py
+++ src/opera_utils/constants.py
@@ -34,6 +34,6 @@ EXTRA_COMPRESSED_TIFF_OPTIONS = (
     *DEFAULT_TIFF_OPTIONS,
     # Note: we're dropping mantissa bits before we do not
     # need prevision for LOS rasters (or incidence)
-    "DISCARD_LSB=6",
+    "NBITS=16",
     "PREDICTOR=2",
 )
