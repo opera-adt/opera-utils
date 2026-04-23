edf20e2267066cf11d440ef88dcf67715ef6b7cd
mgovorcin <marin.govorcin@jpl.nasa.gov>
2026-04-23 10:08:08 -0700

    Revert "Add NISAR geometry, mask, and tropo match utilities" due to precommit test failures

--- src/opera_utils/nisar/__init__.py
+++ src/opera_utils/nisar/__init__.py
@@ -2,10 +2,8 @@
 
 from __future__ import annotations
 
-from ._download import download_gslcs, run_download
-from ._geometry import prepare_incidence_angle
+from ._download import run_download
 from ._gunw_search import search_gunw
-from ._mask import get_gslc_mask, get_gunw_mask
 from ._info import (
     find_intersecting_frames,
     get_frame_latlon_bounds,
@@ -30,13 +28,9 @@ __all__ = [
     "OrbitDirection",
     "OutOfBoundsError",
     "UrlType",
-    "download_gslcs",
     "find_intersecting_frames",
     "get_frame_latlon_bounds",
-    "get_gslc_mask",
-    "get_gunw_mask",
     "get_nisar_bbox",
-    "prepare_incidence_angle",
     "load_gpkg",
     "nisar_frame_info",
     "open_file",
--- src/opera_utils/tropo/__init__.py
+++ src/opera_utils/tropo/__init__.py
@@ -2,12 +2,5 @@
 
 from ._apply import apply_tropo
 from ._crop import crop_tropo
-from ._match import apply_tropo_correction, match_and_apply_tropo, read_reference_point
 
-__all__ = [
-    "apply_tropo",
-    "apply_tropo_correction",
-    "crop_tropo",
-    "match_and_apply_tropo",
-    "read_reference_point",
-]
+__all__ = ["apply_tropo", "crop_tropo"]
